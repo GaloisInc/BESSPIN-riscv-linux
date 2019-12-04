9a115d370d82b02a3232ae8a9941c288dc254f95
Darius Rad <darius@bluespec.com>
2019-12-04 17:50:00 -0500

    Add function to initialize SWIOTLB including physical address of bounce buffer
    This enables the use of reserved memory that is not ZONEDMA for
    the SWIOTLB bounce buffer.

--- include/linux/swiotlb.h
+++ include/linux/swiotlb.h
@@ -36,6 +36,8 @@ int swiotlb_init_with_tbl(char *tlb, unsigned long nslabs, int verbose);
 extern unsigned long swiotlb_nr_tbl(void);
 unsigned long swiotlb_size_or_default(void);
 extern int swiotlb_late_init_with_tbl(char *tlb, unsigned long nslabs);
+extern int swiotlb_late_init_with_tbl_phys(char *tlb, phys_addr_t tlb_phys,
+					   unsigned long nslabs);
 extern void __init swiotlb_update_mem_attributes(void);
 
 /*
